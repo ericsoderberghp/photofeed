c24e617b65b95a533c5384f508b4ad4b388d75b0
Eric Soderberg <eric.soderberg@hpe.com>
2019-08-08 10:57:57 -0700

    Added background tile on Start

--- src/Start.js
+++ src/Start.js
@@ -7,55 +7,69 @@ const Start = ({ onSession }) => {
   const [error, setError] = React.useState();
 
   return (
-    <Box pad="large" align="center">
-      <Heading textAlign="center">Welcome</Heading>
-      <Paragraph textAlign="center">
-        To start with, we need to verify you are you.
-      </Paragraph>
-      <Form
-        data={{ email: '', password: '' }}
-        onSubmit={({ value: user }) => {
-          setBusy(true);
-          const body = JSON.stringify(user);
-          fetch(`${apiUrl}/sessions`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body,
-          })
-            .then((response) => {
-              if (!response.ok) {
-                return response.text()
-                  .then((text) => {
-                    setError(text);
-                    throw new Error(text);
-                  });
-              } else {
-                return response.json();
-              }
-            })
-            .then((session) => {
-              localStorage.setItem('session', JSON.stringify(session));
-              onSession(session);
-            })
-            .catch(() => setBusy(false));
-        }}
+    <Box
+      fill
+      style={{
+        minHeight: '100vh',
+        backgroundImage: 'url(/background-tile.svg)',
+        backgroundRepeat: 'repeat',
+      }}
+    >
+      <Box
+        margin="large"
+        pad="large"
+        align="center"
+        background={{ color: 'accent-3', opacity: 'strong' }}
       >
-        <FormField name="email" placeholder="email" required />
-        <FormField name="password" placeholder="password" type="password" required />
-        {error && (
-          <Box pad="medium" align="center">
-            <Text color="status-critical">{error}</Text>
+        <Heading textAlign="center">Welcome</Heading>
+        <Paragraph textAlign="center">
+          To start with, we need to verify you are you.
+        </Paragraph>
+        <Form
+          data={{ email: '', password: '' }}
+          onSubmit={({ value: user }) => {
+            setBusy(true);
+            const body = JSON.stringify(user);
+            fetch(`${apiUrl}/sessions`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+              },
+              body,
+            })
+              .then((response) => {
+                if (!response.ok) {
+                  return response.text()
+                    .then((text) => {
+                      setError(text);
+                      throw new Error(text);
+                    });
+                } else {
+                  return response.json();
+                }
+              })
+              .then((session) => {
+                localStorage.setItem('session', JSON.stringify(session));
+                onSession(session);
+              })
+              .catch(() => setBusy(false));
+          }}
+        >
+          <FormField name="email" placeholder="email" required />
+          <FormField name="password" placeholder="password" type="password" required />
+          {error && (
+            <Box pad="medium" align="center">
+              <Text color="status-critical">{error}</Text>
+            </Box>
+          )}
+          <Box align="center" margin={{ top: 'large' }}>
+            {busy
+              ? <Text>Just a sec ...</Text>
+              : <Button type="submit" primary label="Start" />
+            }
           </Box>
-        )}
-        <Box align="center" margin={{ top: 'large' }}>
-          {busy
-            ? <Text>Just a sec ...</Text>
-            : <Button type="submit" primary label="Start" />
-          }
-        </Box>
-      </Form>
+        </Form>
+      </Box>
     </Box>
   );
 }
